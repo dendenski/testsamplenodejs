68e8c90150d59a2aef2126e242087b60423aff18
dendenski <dennisbuyco@gmail.com>
2023-05-05 23:53:08 +0800

    test3

--- express/server.js
+++ express/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const mssql = require('mssql');
 const router = express.Router();
 router.get('/', (req, res) => {
   console.log("testing");

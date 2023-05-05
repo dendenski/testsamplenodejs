064650acab9824b35e0b00b45d6a55a3170986a3
dendenski <dennisbuyco@gmail.com>
2023-05-05 23:50:27 +0800

    test2

--- express/server.js
+++ express/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
+  console.log("testing");
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();

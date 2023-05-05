e71a54f99bbd595d5502e9d9b55db04073da127b
dendenski <dennisbuyco@gmail.com>
2023-05-05 22:13:36 +0800

    revert 2

--- express/server.js
+++ express/server.js
@@ -2,20 +2,18 @@
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const bodyParser = require('body-parser');
-const mssql = require('mssql');
-const session = require('express-session');
-const cheerio = require("cheerio");
-const fs = require('fs');
-
-
 const app = express();
+const bodyParser = require('body-parser');
+// const mssql = require('mssql');
+// const session = require('express-session');
+// const cheerio = require("cheerio");
+// const fs = require('fs');
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js!</h1>');
   res.end();
-  // res.sendFile(path.join(__dirname + '../login.html'));
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
